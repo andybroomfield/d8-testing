1f4602fe0563512c39506b5eaf586c25221d3ba8
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2019-08-28 11:37:11 +0100

    Initial Browser test

--- web/modules/custom/tdd_bhcc/tests/src/Functional/PageTestTest.php
+++ web/modules/custom/tdd_bhcc/tests/src/Functional/PageTestTest.php
@@ -1,12 +1,20 @@
 <?php
 // tests/src/Functional/PageListTest.php
 
-namespace Drupal\Tests\tdd_dublin\Functional;
+namespace Drupal\Tests\tdd_bhcc\Functional;
 
-use Drupal\Tests\BrowserTestBase\BrowserTestBase;
+use Drupal\Tests\BrowserTestBase;
 
 class PageListTest extends BrowserTestBase {
 
   protected static $modules = ['tdd_bhcc'];
+
+  /**
+   * Test the Listing Page Exists
+   */
+  public function testListingPageExists() {
+    $this->drupalGet('pages');
+    $this->assertSession()->statusCodeEquals(200);
+  }
 
 }
