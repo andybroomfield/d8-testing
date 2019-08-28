9c280481cf089ef36c64271bdce0c09ec6b670f8
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2019-08-28 11:08:53 +0100

    Add initial test module

---
+++ web/modules/custom/tdd_bhcc/tests/src/Functional/PageTestTest.php
@@ -0,0 +1,12 @@
+<?php
+// tests/src/Functional/PageListTest.php
+
+namespace Drupal\Tests\tdd_dublin\Functional;
+
+use Drupal\Tests\BrowserTestBase\BrowserTestBase;
+
+class PageListTest extends BrowserTestBase {
+
+  protected static $modules = ['tdd_bhcc'];
+
+}
